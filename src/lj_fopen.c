8516f55c9e529cf44776b925fd9201507c4616ef
Wohlstand <admin@wohlnet.ru>
2019-12-12 15:24:20 +0300

    lj_fopen.c: codestyle update
    To match a rest of code here

--- src/lj_fopen.c
+++ src/lj_fopen.c
@@ -1,48 +1,48 @@
 #include "lj_fopen.h"
+
 /*
- * Forces to look for unicode paths on Windows
+ * Forces to look for UTF-8 paths on Windows
+ * (Non-standard optional feature intended for convenience to embed LuaJIT into cross-platform applications)
  */
 #if defined(_WIN32) && defined(LUAJIT_FORCE_UTF8_FOPEN)
 #include <windows.h>
 
 FILE *_lua_fopen(const char *filename, const char *mode)
 {
-    int new_Len1 = 0;
-    int new_Len2 = 0;
-    int fn_len_s = strlen(filename);
-    int m_len_s  = strlen(mode);
-    if(fn_len_s==0) return NULL;
-    if(m_len_s==0) return NULL;
-    wchar_t path[MAX_PATH];
-    wchar_t wmode[MAX_PATH];
-    new_Len1 = MultiByteToWideChar(CP_UTF8, 0, filename, fn_len_s, path, fn_len_s);
-    if(new_Len1>=MAX_PATH) return NULL;
-    path[new_Len1] = L'\0';
-    new_Len2 = MultiByteToWideChar(CP_UTF8, 0, mode, m_len_s, wmode, m_len_s);
-    if(new_Len2>=MAX_PATH) return NULL;
-    wmode[new_Len2] = L'\0';
-    FILE *f = _wfopen(path, wmode);
-    return f;
+  int fn_len_s = strlen(filename), m_len_s = strlen(mode), fn_len_w = 0, m_len_w = 0;
+  wchar_t path[MAX_PATH], wmode[MAX_PATH];
+
+  if (fn_len_s == 0 || m_len_s == 0)
+    return NULL;
+
+  fn_len_w = MultiByteToWideChar(CP_UTF8, 0, filename, fn_len_s, path, fn_len_s);
+  if (fn_len_w >= MAX_PATH) return NULL;
+  path[fn_len_w] = L'\0';
+
+  m_len_w = MultiByteToWideChar(CP_UTF8, 0, mode, m_len_s, wmode, m_len_s);
+  if (m_len_w >= MAX_PATH) return NULL;
+  wmode[m_len_w] = L'\0';
+
+  return _wfopen(path, wmode);
 }
 
 FILE *_lua_freopen(const char *filename, const char *mode, FILE * oldfile)
 {
-    int new_Len1 = 0;
-    int new_Len2 = 0;
-    int fn_len_s = strlen(filename);
-    int m_len_s  = strlen(mode);
-    if(fn_len_s==0) return NULL;
-    if(m_len_s==0) return NULL;
-    wchar_t path[MAX_PATH];
-    wchar_t wmode[MAX_PATH];
-    new_Len1 = MultiByteToWideChar(CP_UTF8, 0, filename, fn_len_s, path, fn_len_s);
-    if(new_Len1>=MAX_PATH) return NULL;
-    path[new_Len1] = L'\0';
-    new_Len2 = MultiByteToWideChar(CP_UTF8, 0, mode, m_len_s, wmode, m_len_s);
-    if(new_Len2>=MAX_PATH) return NULL;
-    wmode[new_Len2] = L'\0';
-    FILE *f = _wfreopen(path, wmode, oldfile);
-    return f;
+  int fn_len_s = strlen(filename), m_len_s = strlen(mode), fn_len_w = 0, m_len_w = 0;
+  wchar_t path[MAX_PATH], wmode[MAX_PATH];
+
+  if (fn_len_s == 0 || m_len_s == 0)
+    return NULL;
+
+  fn_len_w = MultiByteToWideChar(CP_UTF8, 0, filename, fn_len_s, path, fn_len_s);
+  if (fn_len_w >= MAX_PATH) return NULL;
+  path[fn_len_w] = L'\0';
+
+  m_len_w = MultiByteToWideChar(CP_UTF8, 0, mode, m_len_s, wmode, m_len_s);
+  if (m_len_w >= MAX_PATH) return NULL;
+  wmode[m_len_w] = L'\0';
+
+  return _wfreopen(path, wmode, oldfile);
 }
 
 #endif
